30fcd7b86b1c20ed874b973f717bf0e2c31ba989
sandsor <70569977+sandsor@users.noreply.github.com>
2020-09-11 09:21:21 +0200

    Add project files.

---
+++ math_calc/math_calc.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+
+int main()
+{
+    
+	for (int i = 0; i < 101; i++)
+	{
+		std::cout << i << ' ';
+	}
+
+	std::cout << std::endl;
+
+	for (int x = 100; x > 0; x--)
+	{
+		std::cout << x << ' ';
+	}
+}
